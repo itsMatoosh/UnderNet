7651e01f1007eb28287fa01737f2bc55d502c5a6
Mateusz Rębacz <matirebacz@op.pl>
2017-01-10 19:15:47 +0100

    Started work on the underlying network system.

---
+++ app/src/main/java/me/matoosh/undernet/p2p/PeersManager.java
@@ -0,0 +1,26 @@
+package me.matoosh.undernet.p2p;
+
+import java.net.InetAddress;
+
+/**
+ * Manages direct connections to this device.
+ * Created by Mateusz Rębacz on 10.01.2017.
+ */
+
+public class PeersManager {
+    /**
+     * Connects directly to a peer using the specified adress.
+     * @param address
+     */
+    public void connect(InetAddress address) {
+        
+    }
+
+    /**
+     * Called when there is a connection incoming.
+     * @param address
+     */
+    public void onConnection(InetAddress address) {
+
+    }
+}
