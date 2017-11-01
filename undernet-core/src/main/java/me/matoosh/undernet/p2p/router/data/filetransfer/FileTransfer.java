2d9193871fd1763a4372117f97f660bbce170569
Mateusz Rębacz <matirebacz@op.pl>
2017-11-01 22:50:18 +0100

    Removed the testing file suffix

--- undernet-core/src/main/java/me/matoosh/undernet/p2p/router/data/filetransfer/FileTransfer.java
+++ undernet-core/src/main/java/me/matoosh/undernet/p2p/router/data/filetransfer/FileTransfer.java
@@ -95,7 +95,6 @@ public class FileTransfer {
             }
         } else {
             //Creating or replacing the file.
-            file = new File(file.toString() + "-r");
             if (file.exists()) {
                 file.delete();
             }
