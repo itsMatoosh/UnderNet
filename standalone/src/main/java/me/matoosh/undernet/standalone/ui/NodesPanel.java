2ce5c596fbab802f50d3f4584b81de3e6b8e130e
Mateusz Rębacz <matirebacz@op.pl>
2018-04-29 23:43:42 +0200

    Added color coding to the nodes panel

--- standalone/src/main/java/me/matoosh/undernet/standalone/ui/NodesPanel.java
+++ standalone/src/main/java/me/matoosh/undernet/standalone/ui/NodesPanel.java
@@ -1,21 +1,15 @@
 package me.matoosh.undernet.standalone.ui;
 
-import java.awt.Dimension;
-import java.awt.GridLayout;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.nio.channels.Channel;
 import java.util.ArrayList;
+import java.util.HashMap;
 
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JLabel;
-import javax.swing.JList;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.ListSelectionModel;
+import javax.swing.*;
 
 import me.matoosh.undernet.UnderNet;
 import me.matoosh.undernet.event.Event;
@@ -25,6 +19,7 @@ import me.matoosh.undernet.event.cache.NodeCacheAddedEvent;
 import me.matoosh.undernet.event.cache.NodeCacheRemovedEvent;
 import me.matoosh.undernet.event.channel.ChannelClosedEvent;
 import me.matoosh.undernet.event.channel.ChannelCreatedEvent;
+import me.matoosh.undernet.event.channel.message.ChannelMessageReceivedEvent;
 import me.matoosh.undernet.p2p.cache.EntryNodeCache;
 import me.matoosh.undernet.p2p.node.Node;
 import me.matoosh.undernet.p2p.router.InterfaceStatus;
@@ -40,6 +35,11 @@ import me.matoosh.undernet.standalone.ui.dialog.AddNodeCacheDialog;
  */
 
 public class NodesPanel extends JPanel {
+    /**
+     * Nodes that recently sent messages to self.
+     */
+    public ArrayList<Node> receivedMsgFrom = new ArrayList<>();
+
     /**
      * The list of nodes.
      */
@@ -77,6 +77,9 @@ public class NodesPanel extends JPanel {
             }
         });
         add(new JScrollPane(nodesList));
+
+        //Setting cell renderer for the nodes list.
+        nodesList.setCellRenderer(new NodesListCellRenderer(this));
 
         //Adding the buttons on the bottom.
         JPanel buttonsDrawer = new JPanel();
@@ -141,6 +144,14 @@ public class NodesPanel extends JPanel {
                 refreshNodesList();
             }
         }, ChannelClosedEvent.class);
+        EventManager.registerHandler(new EventHandler() {
+            @Override
+            public void onEventCalled(Event e) {
+                //Called when a message has been received.
+                ChannelMessageReceivedEvent messageReceivedEvent = (ChannelMessageReceivedEvent)e;
+                receivedMsgFrom.add(messageReceivedEvent.remoteNode);
+            }
+        }, ChannelMessageReceivedEvent.class);
     }
 
     /**
@@ -177,5 +188,46 @@ public class NodesPanel extends JPanel {
         //The add button was pressed.
         JDialog nodeAddDialog = new AddNodeCacheDialog(UnderNetStandalone.mainAppFrame);
         nodeAddDialog.setVisible(true);
+    }
+}
+class NodesListCellRenderer extends DefaultListCellRenderer
+{
+    private NodesPanel nodesPanel;
+
+    public NodesListCellRenderer(NodesPanel panel) {
+        this.nodesPanel = panel;
+    }
+
+    @Override
+    public Component getListCellRendererComponent(JList list, Object value, int index,
+                                                  boolean isSelected, boolean cellHasFocus) {
+        Component c = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+        if (value instanceof Node) {
+            Node node = (Node) value;
+            setText(node.toString());
+            if(UnderNet.router.status == InterfaceStatus.STARTED) {
+                if (node.isConnected()) {
+                    setBackground(Color.GREEN);
+                    for (int i = 0; i < this.nodesPanel.receivedMsgFrom.size(); i++) {
+                        if (this.nodesPanel.receivedMsgFrom.get(i).address.equals(node.address)) {
+                            setBackground(Color.CYAN);
+                            this.nodesPanel.receivedMsgFrom.remove(node);
+                        }
+                    }
+                } else {
+                    setBackground(Color.RED);
+                }
+            } else {
+                setBackground(Color.ORANGE);
+            }
+
+            if (isSelected) {
+                setBackground(getBackground().darker());
+            }
+        } else {
+            setText("UNKNOWN");
+            setBackground(Color.gray);
+        }
+        return c;
     }
 }
