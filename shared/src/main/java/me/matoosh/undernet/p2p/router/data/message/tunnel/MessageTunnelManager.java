d29d187dc4d1ce3d35fbfe552a26fd4535f8975e
Mateusz Rębacz <matirebacz@op.pl>
2019-01-13 18:15:53 +0100

    Update MessageTunnelManager.java

--- shared/src/main/java/me/matoosh/undernet/p2p/router/data/message/tunnel/MessageTunnelManager.java
+++ shared/src/main/java/me/matoosh/undernet/p2p/router/data/message/tunnel/MessageTunnelManager.java
@@ -12,7 +12,6 @@ import me.matoosh.undernet.p2p.router.Router;
 import me.matoosh.undernet.p2p.router.data.NetworkID;
 import me.matoosh.undernet.p2p.router.data.message.MsgType;
 import me.matoosh.undernet.p2p.router.data.message.NetworkMessage;
-import me.matoosh.undernet.p2p.router.data.resource.transfer.ResourceTransferHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -129,7 +128,7 @@ public class MessageTunnelManager extends Manager {
         //Finding an existing tunnel.
         for (int i = 0; i < messageTunnels.size(); i++) {
             MessageTunnel tunnel = messageTunnels.get(i);
-            if (tunnel.getOrigin().equals(origin) && tunnel.getDestination().equals(destination)) {
+            if (tunnel != null && tunnel.getOrigin().equals(origin) && tunnel.getDestination().equals(destination)) {
                 return tunnel;
             }
         }
