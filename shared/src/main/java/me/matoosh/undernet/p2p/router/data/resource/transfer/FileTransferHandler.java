ceb5b53ce9f8dc4b1e991fcf14278840ef05039d
Mateusz Rębacz <matirebacz@op.pl>
2019-02-28 23:30:36 +0100

    Update FileTransferHandler.java

--- shared/src/main/java/me/matoosh/undernet/p2p/router/data/resource/transfer/FileTransferHandler.java
+++ shared/src/main/java/me/matoosh/undernet/p2p/router/data/resource/transfer/FileTransferHandler.java
@@ -37,9 +37,9 @@ public class FileTransferHandler extends ResourceTransferHandler {
     private long fileLength;
 
     /**
-     * The standard buffer size for file chunks (256kb)
+     * The standard buffer size for file chunks (32kb)
      */
-    public static final int BUFFER_SIZE = 1024 * 256;
+    public static final int BUFFER_SIZE = 1024 * 32;
 
     /**
      * The amount of bytes written from the received chunks.
